2691d48e4b2973c0da2bd651b7ce5a4adb17037f
jahidcx <muhdjahidhasan@gmail.com>
2024-06-10 20:10:51 +0600

    completed module 69

--- index.js
+++ index.js
@@ -84,6 +84,27 @@ async function run() {
             res.send(result);
         })
 
+        app.post('/menu',verifyToken,verifyAdmin, async(req,res)=>{
+            const item = req.body;
+            const result = await menuCollection.insertOne(item);
+            res.send(result); 
+        })
+
+        app.delete('/menu/:id',verifyToken, verifyAdmin, async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await menuCollection.deleteOne(query);
+            res.send(result);
+        })
+
+       
+        app.get('/menu/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await menuCollection.findOne(query);
+            res.send(result);
+        })
+
         app.get('/review', async (req, res) => {
             const result = await reviewCollection.find().toArray();
             res.send(result);
@@ -131,11 +152,9 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/users/admin/:email', verifyToken, verifyAdmin, async (req, res) => {
+        app.get('/users/admin/:email', verifyToken,  async (req, res) => {
             const email = req.params.email;
-            if (email !== req.decoded.email) {
-                return res.status(403).send({ message: "unauthorized access" })
-            }
+            
 
             const query = { email: email };
             const user = await userCollection.findOne(query);
